612befd3f82e3d560691e5ff564737ab95095c89
irina kulakova <irinkul@mail.com>
2022-01-22 20:24:09 +0300

    added Procfile

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,9 @@
+import os
+
+
 class Config:
     SECRET_KEY = "top secret"
     JWT_ACCESS_LIFESPAN = {"hours": 24}
     JWT_REFRESH_LIFESPAN = {"days": 30}
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
