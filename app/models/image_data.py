9210fa7e6c6e83b565edccce6360b45f614fb247
svetlana <svetlana.voroncova.1999@mail.ru>
2022-09-07 00:31:49 +0300

    added applinks verify

--- app/models/image_data.py
+++ app/models/image_data.py
@@ -4,3 +4,5 @@ from .db_init import db
 class ImageData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(1000))
+    title = db.Column(db.String(1000))
+    text = db.Column(db.String(1000))
